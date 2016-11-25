cbec6c5be1effa9266c862b63ba13158eb6f7033
ae4 <ae4@iMac.fios-router.home>
2016-11-25 15:56:50 -0500

    Added 3 Views for List Controller
    Added 3 views: Edit, Details, Create

--- src/AppBundle/Controller/ListController.php
+++ src/AppBundle/Controller/ListController.php
@@ -11,12 +11,32 @@ class ListController extends Controller
     /**
      * @Route("/list", name="user_list")
      */
-    public function userListAction(Request $request)
+    public function userListAction()
     {
-        die('User List')
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('list/index.html.twig');
+    }
+    
+    /**
+     * @Route("/list/create", name="user_create")
+     */
+    public function userCreateAction(Request $request)
+    {
+        return $this->render('list/create.html.twig');
+    }
+    
+    /**
+     * @Route("/list/edit/{id}", name="user_edit")
+     */
+    public function userEditAction($id, Request $request)
+    {
+        return $this->render('list/edit.html.twig');
+    }
+    
+    /**
+     * @Route("/list/details/{id}", name="user_details")
+     */
+    public function userDetailsAction($id)
+    {
+        return $this->render('list/details.html.twig');
     }
 }
